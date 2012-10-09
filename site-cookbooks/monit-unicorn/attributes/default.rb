589f0497bed483bf7e24f2dec9748ab4b5e450f3
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-10-09 14:50:23 +0900

    only use cookbooks, not site-cookbooks

--- site-cookbooks/monit-unicorn/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-
-default[:rails][:path]           = "/home/ubuntu/"
-default[:rails][:user]           = "ubuntu"
-
-default[:unicorn][:max_memory]   = "300.0 MB"
-default[:unicorn][:workers]      = "15"
--- site-cookbooks/teammight/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-
-default[:teammight][:app]  = "RailsApp"
-
