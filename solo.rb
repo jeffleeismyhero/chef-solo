def334d94993348392bc2a5782a64ba40aab81d2
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-10-18 09:47:51 +0900

    minimal set for bootstraping

--- solo.rb
+++ solo.rb
@@ -1,8 +1,6 @@
 root = File.absolute_path(File.dirname(__FILE__))
  
 file_cache_path root
-cookbook_path [root + '/cookbooks']
-role_path root + "/roles"
-data_bag_path root + "/data_bags"
+cookbook_path '/tmp'
 log_level :info
 log_location STDOUT
