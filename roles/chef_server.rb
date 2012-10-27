941208bc6b08cac2e9b298e636aa0f461e1fa29b
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-10-27 09:45:43 +0900

    first commit of chef-server

--- roles/chef_server.rb
+++ roles/chef_server.rb
@@ -2,12 +2,15 @@ name "chef_server"
 description "The Provisioning  server"
 
 default_attributes   "chef_server"  => {
-    "url": "http://localhost.localdomain:4000",
-    "webui_enabled": true
+    "url" =>  "http://localhost.localdomain:4000",
+    "webui_enabled" =>  true
 }
 
 run_list(
-  "recipe[chef-server::rubygems-install]"
+ [
+  "recipe[chef-server::rubygems-install]",
+  "recipe[chef-server::nginx-proxy]"
+ ]
 )
 
 
