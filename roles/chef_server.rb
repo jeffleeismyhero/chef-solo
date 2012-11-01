1dfc534a6de3621541caabe1f06e377708f2818d
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-11-01 18:09:18 +0900

    chef-repo files from Opscode

--- roles/chef_server.rb
+++ roles/chef_server.rb
@@ -2,9 +2,12 @@ name "chef_server"
 description "The Provisioning  server"
 
 default_attributes   "chef_server"  => {
-    "url" =>  "http://localhost.localdomain:4000",
-    "webui_enabled" =>  true
+  "url" =>  "http://192.168.56.52:4000",
+  "webui_enabled" =>  true,
+  'ssl_req'  => "/C=US/ST=Several/L=Locality/O=Example/OU=Operations/CN=chef-server-proxy/emailAddress=root@localhost"
 }
+
+
 
 run_list(
  [
