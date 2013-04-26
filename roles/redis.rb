4de7935d100a4d8c1c1fbc20e717f6605dbd2b0d
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-04-26 16:39:44 +0900

    Add bind to localhost for Redis
    - only used by Sidekiq on local machine

--- roles/redis.rb
+++ roles/redis.rb
@@ -4,7 +4,8 @@ description "Redis key-value store"
 default_attributes :redis => {
   "daemonize" => "yes",
   "pidfile"   => "/var/run/redis/redis-server.pid",
-  'config_path'  => "/etc/redis.conf"
+  'config_path'  => "/etc/redis.conf",
+  'bind' => '127.0.0.1'
 }
 
 run_list ["recipe[redis]"] 
