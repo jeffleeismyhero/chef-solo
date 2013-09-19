70e610fb2640edeed827681bf5f66f2fbe848398
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-09-19 18:59:37 +0900

    Remove unused stuff, configure to use .ruby-version.

--- roles/tm_developer.rb
+++
@@ -1,9 +0,0 @@
-name 'tm_developer'
-
-description "Web server for development includes web server and
-necessary components, application repository form Github"
-
-run_list(
-        "role[tm_web_server]",
-        "recipe[teammight::developer]"
-         )
