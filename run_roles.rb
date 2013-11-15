d6bfd45ec8dcee5df826a88e875ed21d387023f1
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-11-15 17:19:03 +0900

    Script to merge and run server roles from databag in one go.

---
+++ run_roles.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require 'chef/application/solo'
+require 'chef/data_bag_item'
+require 'json'
+require 'pty'
+
+current_path = File.expand_path(File.dirname( __FILE__))
+
+Chef::Config[:solo] = true
+Chef::Config[:data_bag_path] = "#{current_path}/data_bags"
+current_host = ARGV[0].gsub(/\./,'_')
+
+exit unless current_host
+
+begin
+  roles =  Chef::DataBagItem.load(:roles_bag, current_host)["roles"]
+rescue
+  puts "Databag configuration for host #{ARGV[0]} not found"
+  exit
+end
+
+# TODO: Currently only run_list's merged, make it handle all JSON
+# atributes.
+run_lists = []
+roles.map{ |x| "#{ current_path}/#{x}.json"}.each do |role|
+  run_lists << JSON.parse(File.read role)["run_list"] if File.exists? role
+end
+
+File.open("#{current_path}/#{current_host}.json", "w") do |f|
+  f.print({ run_list: run_lists.compact.uniq }.to_json)
+end
+
+cmd = "cd #{current_path} && chef-solo --config solo.rb --json-attributes #{current_host}.json"
+begin
+  PTY.spawn (cmd) do |stdin, stout, pid|
+    begin
+      stdin.each { |line| print line }
+    rescue Errno::EIO
+    end
+  end
+rescue PTY::ChildExited
+  puts "The child process exited!"
+end
