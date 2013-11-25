575542185f4a3f5e4534bcab92d401cf6059f170
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-11-25 14:25:10 +0900

    Less verbosity for chef-solo.

--- solo.rb
+++ solo.rb
@@ -16,5 +16,6 @@ file_cache_path root
 cookbook_path ["#{root}/cookbooks", "#{root}/site-cookbooks"]
 role_path     "#{root}/roles"
 data_bag_path "#{root}/data_bags"
-log_level :info
+#log_level :info
+log_level :warn
 log_location STDOUT
